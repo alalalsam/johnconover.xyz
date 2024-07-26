a1bfb2109192809e0f9729eac12f35f1f7b10682
alalalsam <jconover456@gmail.com>
2024-07-26 14:02:57 -0500

    added video and image support to ProjectBlock

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,8 @@ import {
    Avatar,
    Heading,
    Paragraph,
+   Image,
+   Video,
    Anchor,
    Button,
    Layer,
@@ -193,21 +195,21 @@ const App = () => {
 							</Box>
 							
 							<ProjectBlock 
-									heading="Chinese" 
-									content="im insane! I'm deranged! don't let me near you! I will 
-									kill you!
-									Im mixed race chinese/white, can I say slurs for both or neither?
-									If I rubbed a magic lamp and a genie popped out, I would have a few
-									wishes. One of these wishes would be that league of legends would 
-									never have 
-									been created. The next wish would be for all food to taste really good. My last
-									and final wish would be big muscles. oh wait, I already
-									have big muscles." 
+								heading="Chinese" 
+								content="im insane! I'm deranged! don't let me near you! I will 
+								kill you! Im mixed race chinese/white, can I say slurs for both or neither?
+								If I rubbed a magic lamp and a genie popped out, I would have a few
+								wishes. One of these wishes would be that league of legends would 
+								never have been created. The next wish would be for all food to taste really good. My last
+								and final wish would be big muscles. oh wait, I already
+								have big muscles." 
+								video="https://www.youtube.com/embed/bI0vq6rDxpc?si=KNuJ8MpdwJpjlzMi"
 							/>
 							
 							<ProjectBlock 
-									heading="Chinese" 
-									content="Chinese Chinese Chinese, anything goes, even Chinese" 
+								heading="Chinese" 
+								content="Chinese Chinese Chinese, anything goes, even Chinese"
+								picture="https://www.uschina.org/sites/default/files/202210/xijinping.jpg"
 							/>
 							
 					</PageContent>
--- src/components/ProjectBlock.tsx
+++ src/components/ProjectBlock.tsx
@@ -1,31 +1,46 @@
 import React from 'react';
-import { Box, Heading, Paragraph } from 'grommet';
+import { Box, Heading, Paragraph, Image, Video } from 'grommet';
 
-const ProjectBlock = ({ heading, content }) => (
-  <Box
-    align="center"
-    justify="center"
-    margin={{ top: "small" }}
-    border={{ color: "active-background", side: "top", size: "small" }}
-    gap="medium"
-  >
-    <Box align="start" justify="center" fill="horizontal">
-      <Heading
-        margin={{ vertical: "small" }}
-        size="medium"
-      >
-        {heading}
-      </Heading>
-      <Paragraph
-        size="large"
-        margin={{ vertical: "xsmall" }}
-        fill
-        color="text-paragraph"
-      >
-        {content}
-      </Paragraph>
-    </Box>
-  </Box>
+const ProjectBlock = ({ heading, content, picture, video }) => (
+	<Box
+		align="center"
+		justify="center"
+		margin={{ top: "small" }}
+		border={{ color: "active-background", side: "top", size: "small" }}
+		gap="medium"
+	>
+		<Box align="start" justify="center" fill="horizontal">
+			<Heading
+				margin={{ vertical: "small" }}
+				size="small"
+			>
+			{heading}
+			</Heading>
+		  
+			{picture && (
+				<Box margin={{ vertical: "small" }}>
+					<Image src={picture} fit="cover" />
+				</Box>
+			)}
+		  
+			{video && (
+				<Box margin={{ vertical: "small" }}>
+					<Video controls="below" fit="cover">
+					<source src={video} type="video/mp4" />
+					</Video>
+				</Box>
+			)}
+		  
+			<Paragraph
+				size="large"
+				margin={{ vertical: "xsmall" }}
+				fill
+				color="text-paragraph"
+			>
+				{content}
+			</Paragraph>
+		</Box>
+	</Box>
 );
 
 export default ProjectBlock;
