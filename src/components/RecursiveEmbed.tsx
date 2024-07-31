faf467b2917f566a1b5ef6853ef4b735741e9ad7
alalalsam <jconover456@gmail.com>
2024-07-31 17:51:04 -0500

    added border to website

--- src/components/RecursiveEmbed.tsx
+++ src/components/RecursiveEmbed.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import { Box } from 'grommet';
 
-const RecursiveEmbed: React.FC<{ depth?: number, maxDepth?: number }> = ({ depth = 0, maxDepth = 50 }) => {
+const RecursiveEmbed: React.FC<{ depth?: number, maxDepth?: number }> = ({ depth = 0, maxDepth = 30 }) => {
 	if (depth >= maxDepth) {
 		return null; // Stop recursion if maxDepth is reached or exceeded
 	}
 	
 	return (
-		<Box style={{ width: '780px', height: '400px', border: 'none' }} pad="medium" border={{ color: 'brand', size: 'small' }} overflow="hidden">
+		<Box style={{ width: '780px', height: '400px' }} pad="medium" border={{ color: 'brand', size: 'small' }} overflow="hidden">
 			<iframe
-				//src={`johnconover.xyz?depth=${depth + 1}&maxDepth=${maxDepth}`}
-				src={`johnconover.xyz`}
+				src={`johnconover.xyz?depth=${depth + 1}&maxDepth=${maxDepth}`}
 				style={{ width: '100%', height: '100%', border: 'none' }}
 				title="Recursive Embed"
 			/>
