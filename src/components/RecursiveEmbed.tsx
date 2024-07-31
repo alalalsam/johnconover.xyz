0d4f81e4e5a3b2b4837ea4c560047310eb0aa5b3
alalalsam <jconover456@gmail.com>
2024-07-31 18:09:29 -0500

    am i stupid?

--- src/components/RecursiveEmbed.tsx
+++ src/components/RecursiveEmbed.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Box } from 'grommet';
 
-const RecursiveEmbed: React.FC<{ depth?: number, maxDepth?: number }> = ({ depth = 0, maxDepth =  30}) => {
+const RecursiveEmbed: React.FC<{ depth?: number, maxDepth?: number }> = ({ depth = 0, maxDepth =  }) => {
 	if (depth >= maxDepth) {
 		return null; // Stop recursion if maxDepth is reached or exceeded
 	}
@@ -9,7 +9,7 @@ const RecursiveEmbed: React.FC<{ depth?: number, maxDepth?: number }> = ({ depth
 	return (
 		<Box style={{ width: '780px', height: '400px' }} pad="medium" border={{ color: 'brand', size: 'small' }} overflow="hidden">
 			<iframe
-				src={`https://johnconover.xyz`}
+				src={`https://johnconover.xyz?depth=${depth + 1}&maxDepth=${maxDepth}`}
 				style={{ width: '100%', height: '100%', border: 'none' }}
 				title="Recursive Embed"
 			/>
